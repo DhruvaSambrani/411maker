6b198a24ff8a79ea8100e5a9441ba7d0f5d03b3b
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2021-09-15 19:24:05 +0530

    add publish, fix scrips

--- maker.sh
+++ maker.sh
@@ -6,19 +6,42 @@ stripext() {
 
 compile() {
     name=$(stripext "$1")
+    fname=$(stripext $(basename "$1"))
+    calldir=$(pwd)
+    cd $(dirname $1)
+
     echo "Making $name..." >> /dev/stderr
 
-    echo "Weaveing to $name.pandoc.md"  >> /dev/stderr
-    pweave "$1" -F "$(dirname "$1")/figures" -f pandoc -o "$name.pandoc.md" >> /dev/stderr
+    echo "Weaveing to $fname.pandoc.md"  >> /dev/stderr
+    pweave "$fname.md" -f pandoc -o "$fname.pandoc.md" >> /dev/stderr
 
     echo "Adding code numbers" >> /dev/stderr
-    sed -i -E "s/\`\`\`python/\`\`\`{.python .numberLines}/g" "$name.pandoc.md"
+    sed -i -E "s/\`\`\`python/\`\`\`{.python .numberLines}/g" "$fname.pandoc.md"
 
     echo "running pandoc with custom template and design..." >> /dev/stderr
-    pandoc -i $name.pandoc.md -f markdown -t pdf -o $name.pdf --template=template.latex --highlight-style=style.theme >> /dev/stderr
+    pandoc -i "$fname.pandoc.md" -f markdown -o "$fname.tex" --template="$calldir/template.latex" --highlight-style="$calldir/style.theme" >> /dev/stderr
+    
+    echo "running pandoc to make pdf" >> /dev/stderr
+    pandoc -i "$fname.pandoc.md" -f markdown -t pdf -o "$fname.pdf" --template="$calldir/template.latex" --highlight-style="$calldir/style.theme" >> /dev/stderr
+    
+    cd "$calldir"
 
     echo "Output at (you can pipe this):" >> /dev/stderr
     echo "$name.pdf"
+}
+
+publish() {
+    name=$(stripext "$1")
+    echo "Compiling again..." >> /dev/stderr
+    compile $1 >> /dev/stderr
+    echo -e "Compiled!\n" >> /dev/stderr
+    echo -e "Tangling..." >> /dev/stderr
+    ptangle $1 >> /dev/stderr
+    echo -e "Tangled!\n" >> /dev/stderr
+    echo -e "Packaging..." >> /dev/stderr
+    tar czvf "$name.tar.gz" "$1" "$name.py" "$name.pdf" "$name.tex" >> /dev/stderr
+    echo "COMPLETE! tar.gz created at " >> /dev/stderr
+    echo "$name.tar.gz"
 }
 
 help() {
@@ -32,6 +55,7 @@ help() {
 case "${1}" in
     new) mkdir l$2 && echo -e "---\nlabno=$2\ndate=\\\\today\n---\n\n" > l$2/content.md ;;
     compile) compile "$2" ;;
+    publish) publish "$2" ;;
     *) help ;;
 esac
 
